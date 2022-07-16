6a760efa50bc1d4f41bb8bbaec93defd7c025fcf
Hichem Fantar <hichemfantar2049@gmail.com>
2022-07-16 15:27:08 +0100

    change gh link

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -175,7 +175,7 @@ const Nav = () => (
 		</Menu>
 		<Github>
 			<a
-				href="https://github.com/bchiang7/spotify-profile"
+				href="https://github.com/hichemfantar/spotify-observer"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
