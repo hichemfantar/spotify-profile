46ee68455446ec13482ecd69367fc09612b86808
Hichem Fantar <hichemfantar2049@gmail.com>
2022-07-16 18:12:30 +0100

    refactor axios and add react query

---
+++ client/src/appConstants.js
@@ -0,0 +1,9 @@
+const appConstants = {
+	spotify: {
+		api: {
+			url: "https://api.spotify.com/v1/",
+		},
+	},
+};
+
+export default appConstants;
--- client/src/spotify/index.js
+++ client/src/spotify/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import appConstants from "../appConstants";
 import { getHashParams } from "../utils";
 
 // TOKENS ******************************************************************************************
@@ -20,9 +21,8 @@ const getLocalAccessToken = () =>
 const getLocalRefreshToken = () =>
 	window.localStorage.getItem("spotify_refresh_token");
 
-const axiosClient = axios.create({
-	// baseURL: process.env.REACT_APP_DOMAIN,
-	// withCredentials: true,
+const axiosSpotifyClient = axios.create({
+	baseURL: appConstants.spotify.api.url,
 	headers: {
 		// Authorization: `Bearer ${token}`,
 		"Content-Type": "application/json",
@@ -32,7 +32,7 @@ const axiosClient = axios.create({
 // Refresh the token
 const refreshAccessToken = async () => {
 	try {
-		const { data } = await axiosClient.get(
+		const { data } = await axios.get(
 			`/refresh_token?refresh_token=${getLocalRefreshToken()}`
 		);
 		const { access_token } = data;
@@ -85,144 +85,247 @@ export const logout = () => {
 // const headers = {
 // 	Authorization: `Bearer ${token}`,
 // };
-axiosClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+axiosSpotifyClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 /**
  * Get Current User's Profile
  * https://developer.spotify.com/documentation/web-api/reference/users-profile/get-current-users-profile/
  */
-export const getUser = () => axiosClient.get("https://api.spotify.com/v1/me");
+export const getUser = () => axiosSpotifyClient.get("me");
+
+export function useGetUser() {
+	return useQuery(["user"], async () => {
+		return getUser().then((res) => res.data);
+	});
+}
 
 /**
  * Get User's Followed Artists
  * https://developer.spotify.com/documentation/web-api/reference/follow/get-followed/
  */
 export const getFollowing = () =>
-	axiosClient.get("https://api.spotify.com/v1/me/following?type=artist");
+	axiosSpotifyClient.get("me/following?type=artist");
+
+export function useGetFollowing() {
+	return useQuery(["following"], async () => {
+		return getFollowing().then((res) => res.data);
+	});
+}
 
 /**
  * Get Current User's Recently Played Tracks
  * https://developer.spotify.com/documentation/web-api/reference/player/get-recently-played/
  */
 export const getRecentlyPlayed = () =>
-	axiosClient.get("https://api.spotify.com/v1/me/player/recently-played");
+	axiosSpotifyClient.get("me/player/recently-played");
+
+export function useGetRecentlyPlayed() {
+	return useQuery(["RecentlyPlayed"], async () => {
+		return getRecentlyPlayed().then((res) => res.data);
+	});
+}
 
 /**
  * Get a List of Current User's Playlists
  * https://developer.spotify.com/documentation/web-api/reference/playlists/get-a-list-of-current-users-playlists/
  */
-export const getPlaylists = () =>
-	axiosClient.get("https://api.spotify.com/v1/me/playlists");
+export const getPlaylists = () => axiosSpotifyClient.get("me/playlists");
+
+export function useGetPlaylists() {
+	return useQuery(["Playlists"], async () => {
+		return getPlaylists().then((res) => res.data);
+	});
+}
 
 /**
  * Get a User's Top Artists
  * https://developer.spotify.com/documentation/web-api/reference/personalization/get-users-top-artists-and-tracks/
  */
 export const getTopArtistsShort = () =>
-	axiosClient.get(
-		"https://api.spotify.com/v1/me/top/artists?limit=50&time_range=short_term"
-	);
+	axiosSpotifyClient.get("me/top/artists?limit=50&time_range=short_term");
+
+export function useGetTopArtistsShort() {
+	return useQuery(["TopArtistsShort"], async () => {
+		return getTopArtistsShort().then((res) => res.data);
+	});
+}
+
 export const getTopArtistsMedium = () =>
-	axiosClient.get(
-		"https://api.spotify.com/v1/me/top/artists?limit=50&time_range=medium_term"
-	);
+	axiosSpotifyClient.get("me/top/artists?limit=50&time_range=medium_term");
+
+export function useGetTopArtistsMedium() {
+	return useQuery(["TopArtistsMedium"], async () => {
+		return getTopArtistsMedium().then((res) => res.data);
+	});
+}
+
 export const getTopArtistsLong = () =>
-	axiosClient.get(
-		"https://api.spotify.com/v1/me/top/artists?limit=50&time_range=long_term"
-	);
+	axiosSpotifyClient.get("me/top/artists?limit=50&time_range=long_term");
+
+export function useGetTopArtistsLong() {
+	return useQuery(["TopArtistsLong"], async () => {
+		return getTopArtistsLong().then((res) => res.data);
+	});
+}
 
 /**
  * Get a User's Top Tracks
  * https://developer.spotify.com/documentation/web-api/reference/personalization/get-users-top-artists-and-tracks/
  */
 export const getTopTracksShort = () =>
-	axiosClient.get(
-		"https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term"
-	);
+	axiosSpotifyClient.get("me/top/tracks?limit=50&time_range=short_term");
+
+export function useGetTopTracksShort() {
+	return useQuery(["TopTracksShort"], async () => {
+		return getTopTracksShort().then((res) => res.data);
+	});
+}
+
 export const getTopTracksMedium = () =>
-	axiosClient.get(
-		"https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term"
-	);
+	axiosSpotifyClient.get("me/top/tracks?limit=50&time_range=medium_term");
+
+export function useGetTopTracksMedium() {
+	return useQuery(["TopTracksMedium"], async () => {
+		return getTopTracksMedium().then((res) => res.data);
+	});
+}
+
 export const getTopTracksLong = () =>
-	axiosClient.get(
-		"https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term"
-	);
+	axiosSpotifyClient.get("me/top/tracks?limit=50&time_range=long_term");
+
+export function useGetTopTracksLong() {
+	return useQuery(["TopTracksLong"], async () => {
+		return getTopTracksLong().then((res) => res.data);
+	});
+}
 
 /**
  * Get an Artist
  * https://developer.spotify.com/documentation/web-api/reference/artists/get-artist/
  */
 export const getArtist = (artistId) =>
-	axiosClient.get(`https://api.spotify.com/v1/artists/${artistId}`);
+	axiosSpotifyClient.get(`artists/${artistId}`);
+
+export function useGetArtist(artistId) {
+	return useQuery(["Artist", artistId], async () => {
+		return getArtist(artistId).then((res) => res.data);
+	});
+}
 
 /**
  * Follow an Artist
  * https://developer.spotify.com/documentation/web-api/reference/follow/follow-artists-users/
  */
 export const followArtist = (artistId) => {
-	const url = `https://api.spotify.com/v1/me/following?type=artist&ids=${artistId}`;
-	return axiosClient.put(url);
+	const url = `me/following?type=artist&ids=${artistId}`;
+	return axiosSpotifyClient.put(url);
 };
+
+export function useFollowArtist(artistId) {
+	return useMutation(["followArtist", artistId], async () => {
+		return followArtist(artistId).then((res) => res.data);
+	});
+}
 
 /**
  * Check if Current User Follows Artists
  * https://developer.spotify.com/documentation/web-api/reference/follow/follow-artists-users/
  */
 export const doesUserFollowArtist = (artistId) =>
-	axiosClient.get(
-		`https://api.spotify.com/v1/me/following/contains?type=artist&ids=${artistId}`
-	);
+	axiosSpotifyClient.get(`me/following/contains?type=artist&ids=${artistId}`);
+
+export function useDoesUserFollowArtist(artistId) {
+	return useQuery(["doesUserFollowArtist", artistId], async () => {
+		return doesUserFollowArtist(artistId).then((res) => res.data);
+	});
+}
 
 /**
  * Check if Users Follow a Playlist
  * https://developer.spotify.com/documentation/web-api/reference/follow/follow-artists-users/
  */
 export const doesUserFollowPlaylist = (playlistId, userId) =>
-	axiosClient.get(
-		`https://api.spotify.com/v1/playlists/${playlistId}/followers/contains?ids=${userId}`
+	axiosSpotifyClient.get(
+		`playlists/${playlistId}/followers/contains?ids=${userId}`
 	);
+
+export function useDoesUserFollowPlaylist(playlistId, userId) {
+	return useQuery(["doesUserFollowPlaylist", playlistId, userId], async () => {
+		return doesUserFollowPlaylist(playlistId, userId).then((res) => res.data);
+	});
+}
 
 /**
  * Create a Playlist (The playlist will be empty until you add tracks)
  * https://developer.spotify.com/documentation/web-api/reference/playlists/create-playlist/
  */
 export const createPlaylist = (userId, name) => {
-	const url = `https://api.spotify.com/v1/users/${userId}/playlists`;
+	const url = `users/${userId}/playlists`;
 	const data = JSON.stringify({ name });
-	return axiosClient.post(url, data);
+	return axiosSpotifyClient.post(url, data);
 };
+
+export function useCreatePlaylist(userId, name) {
+	return useMutation(["createPlaylist", userId, name], async () => {
+		return createPlaylist(userId, name).then((res) => res.data);
+	});
+}
 
 /**
  * Add Tracks to a Playlist
  * https://developer.spotify.com/documentation/web-api/reference/playlists/add-tracks-to-playlist/
  */
 export const addTracksToPlaylist = (playlistId, uris) => {
-	const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=${uris}`;
-	return axiosClient.post(url);
+	const url = `playlists/${playlistId}/tracks?uris=${uris}`;
+	return axiosSpotifyClient.post(url);
 };
+
+export function useAddTracksToPlaylist(playlistId, uris) {
+	return useMutation(["addTracksToPlaylist", playlistId, uris], async () => {
+		return addTracksToPlaylist(playlistId, uris).then((res) => res.data);
+	});
+}
 
 /**
  * Follow a Playlist
  * https://developer.spotify.com/documentation/web-api/reference/follow/follow-playlist/
  */
 export const followPlaylist = (playlistId) => {
-	const url = `https://api.spotify.com/v1/playlists/${playlistId}/followers`;
-	return axiosClient.put(url);
+	const url = `playlists/${playlistId}/followers`;
+	return axiosSpotifyClient.put(url);
 };
+
+export function useFollowPlaylist(playlistId) {
+	return useMutation(["useFollowPlaylist", playlistId], async () => {
+		return followPlaylist(playlistId).then((res) => res.data);
+	});
+}
 
 /**
  * Get a Playlist
  * https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlist/
  */
 export const getPlaylist = (playlistId) =>
-	axiosClient.get(`https://api.spotify.com/v1/playlists/${playlistId}`);
+	axiosSpotifyClient.get(`playlists/${playlistId}`);
+
+export function useGetPlaylist(playlistId) {
+	return useQuery(["Playlist", playlistId], async () => {
+		return getPlaylist(playlistId).then((res) => res.data);
+	});
+}
 
 /**
  * Get a Playlist's Tracks
  * https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlists-tracks/
  */
 export const getPlaylistTracks = (playlistId) =>
-	axiosClient.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`);
+	axiosSpotifyClient.get(`playlists/${playlistId}/tracks`);
+
+export function useGetPlaylistTracks(playlistId) {
+	return useQuery(["PlaylistTracks", playlistId], async () => {
+		return getPlaylistTracks(playlistId).then((res) => res.data);
+	});
+}
 
 /**
  * Return a comma separated string of track IDs from the given array of tracks
@@ -235,10 +338,14 @@ const getTrackIds = (tracks) => tracks.map(({ track }) => track.id).join(",");
  */
 export const getAudioFeaturesForTracks = (tracks) => {
 	const ids = getTrackIds(tracks);
-	return axiosClient.get(
-		`https://api.spotify.com/v1/audio-features?ids=${ids}`
-	);
+	return axiosSpotifyClient.get(`audio-features?ids=${ids}`);
 };
+
+export function useGetAudioFeaturesForTracks(tracks) {
+	return useQuery(["AudioFeaturesForTracks", tracks], async () => {
+		return getAudioFeaturesForTracks(tracks).then((res) => res.data);
+	});
+}
 
 /**
  * Get Recommendations Based on Seeds
@@ -250,31 +357,55 @@ export const getRecommendationsForTracks = (tracks) => {
 	const seed_artists = "";
 	const seed_genres = "";
 
-	return axiosClient.get(
-		`https://api.spotify.com/v1/recommendations?seed_tracks=${seed_tracks}&seed_artists=${seed_artists}&seed_genres=${seed_genres}`
+	return axiosSpotifyClient.get(
+		`recommendations?seed_tracks=${seed_tracks}&seed_artists=${seed_artists}&seed_genres=${seed_genres}`
 	);
 };
+
+export function useGetRecommendationsForTracks(tracks) {
+	return useQuery(["RecommendationsForTracks", tracks], async () => {
+		return getRecommendationsForTracks(tracks).then((res) => res.data);
+	});
+}
 
 /**
  * Get a Track
  * https://developer.spotify.com/documentation/web-api/reference/tracks/get-track/
  */
 export const getTrack = (trackId) =>
-	axiosClient.get(`https://api.spotify.com/v1/tracks/${trackId}`);
+	axiosSpotifyClient.get(`tracks/${trackId}`);
+
+export function useGetTrack(trackId) {
+	return useQuery(["Track", trackId], async () => {
+		return getTrack(trackId).then((res) => res.data);
+	});
+}
 
 /**
  * Get Audio Analysis for a Track
  * https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-analysis/
  */
 export const getTrackAudioAnalysis = (trackId) =>
-	axiosClient.get(`https://api.spotify.com/v1/audio-analysis/${trackId}`);
+	axiosSpotifyClient.get(`audio-analysis/${trackId}`);
+
+export function useGetTrackAudioAnalysis(trackId) {
+	return useQuery(["TrackAudioAnalysis", trackId], async () => {
+		return getTrackAudioAnalysis(trackId).then((res) => res.data);
+	});
+}
 
 /**
  * Get Audio Features for a Track
  * https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/
  */
 export const getTrackAudioFeatures = (trackId) =>
-	axiosClient.get(`https://api.spotify.com/v1/audio-features/${trackId}`);
+	axiosSpotifyClient.get(`audio-features/${trackId}`);
+
+export function useGetTrackAudioFeatures(trackId) {
+	return useQuery(["TrackAudioFeatures", trackId], async () => {
+		return getTrackAudioFeatures(trackId).then((res) => res.data);
+	});
+}
 
 export const getUserInfo = () =>
 	Promise.all([
@@ -293,19 +424,7 @@ export const getUserInfo = () =>
 
 export function useUserInfo() {
 	return useQuery(["user-info"], async () => {
-		return Promise.all([
-			getUser(),
-			getFollowing(),
-			getPlaylists(),
-			getTopArtistsLong(),
-			getTopTracksLong(),
-		]).then(([user, followedArtists, playlists, topArtists, topTracks]) => ({
-			user: user.data,
-			followedArtists: followedArtists.data,
-			playlists: playlists.data,
-			topArtists: topArtists.data,
-			topTracks: topTracks.data,
-		}));
+		return getUserInfo();
 	});
 }
 
@@ -322,14 +441,6 @@ export const getTrackInfo = (trackId) =>
 
 export function useTrackInfo(trackId) {
 	return useQuery(["track-info", trackId], async () => {
-		return Promise.all([
-			getTrack(trackId),
-			getTrackAudioAnalysis(trackId),
-			getTrackAudioFeatures(trackId),
-		]).then(([track, audioAnalysis, audioFeatures]) => ({
-			track: track.data,
-			audioAnalysis: audioAnalysis.data,
-			audioFeatures: audioFeatures.data,
-		}));
+		return getTrackInfo(trackId);
 	});
 }
